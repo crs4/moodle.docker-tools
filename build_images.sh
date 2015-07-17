1ec940c1c1aa9f733c97d9a51527df5c4c92968a
Marco Enrico Piras <kikkomep@crs4.it>
2015-07-17 05:46:34 +0200

    build_images now calls build_image script of every image

--- build_images.sh
+++ build_images.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
-# Build Moodle image
-docker build -t moodle images/moodle/
+# Images to build
+images=("mysql moodle")
+
+# Path of images
+images_path="$(pwd)/images"
+
+# Build images
+for image in ${images[@]}; do
+	cd "${images_path}/${image}"
+	"./build_image.sh"
+done
