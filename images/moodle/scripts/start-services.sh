c672d051919736bd97175dfb99347b62a1be1185
Marco Enrico Piras <kikkomep@crs4.it>
2015-07-18 11:01:32 +0200

    Added index.html for auto-redirecting to /moodle

--- images/moodle/scripts/start-services.sh
+++ images/moodle/scripts/start-services.sh
@@ -6,6 +6,16 @@ set -e
 if [[ ! "$(ls ${MOODLE_WWW_ROOT})" ]]; then
     echo "Copying Moodle ..."
     cp -r -v /opt/moodle ${WWW_ROOT}
+    cat > ${WWW_ROOT}/index.html <<- EOM
+<html>
+    <head>
+        <meta http-equiv="refresh" content="0; url=/moodle">
+    </head>
+<body></body>
+</html>
+EOM
+
+
 else
     echo "Moodle folder exists..."
 fi
