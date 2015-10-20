585fe463635c59355171f3d033bf81a32b44480c
Marco Enrico Piras <kikkomep@crs4.it>
2015-10-20 15:34:23 +0200

    Added log dirs

--- config.sh
+++ config.sh
@@ -5,6 +5,7 @@ export SHARED_HOST_FOLDER="${HOME}/Sharing/MoodleDocker"
 
 # Shared www and data paths
 export SHARED_WWW="${SHARED_HOST_FOLDER}/www"
+export SHARED_LOG="${SHARED_HOST_FOLDER}/log"
 export SHARED_DATA="${SHARED_HOST_FOLDER}/data"
 
 # MySQL Configurations
@@ -19,13 +20,14 @@ export MYSQL_DATADIR="${SHARED_DATA}/mysql"
 export APACHE_WWW_ROOT="/var/www/html"
 export MOODLE_WWW_ROOT="${APACHE_WWW_ROOT}/moodle"
 export MOODLE_DATA_DIR="/data/moodle"
+export MOODLE_LOG_DIR="/var/log/apache2"
 export SHARED_MOODLE_WWW_ROOT="${SHARED_WWW}/moodle"
+export SHARED_MOODLE_LOG_DIR="${SHARED_LOG}/moodle"
 export SHARED_MOODLE_DATA_DIR="${SHARED_DATA}/moodle"
-
-
 
 # SSH service of the Moodle container # TODO: needed?
 CONTAINER_SSH_PORT=4376
+
   #--> SSH KEY <--
 SSH_KEY="${HOME}/.ssh/id_dsa.pub"
 if [[ ! -f ${SSH_KEY} ]]; then
