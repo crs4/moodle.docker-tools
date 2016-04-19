fa396f39cc586ad15db38abdca73a6933fd872b7
Marco Enrico Piras <kikkomep@crs4.it>
2016-04-19 16:55:56 +0200

    fix permissions of the Moodle directory

--- images/moodle/scripts/start-services.sh
+++ images/moodle/scripts/start-services.sh
@@ -6,6 +6,7 @@ set -e
 if [[ ! "$(ls ${MOODLE_WWW_ROOT})" ]]; then
     echo "Copying Moodle ..."
     cp -r -v /opt/moodle ${WWW_ROOT}
+    chown -R www-data:www-data ${WWW_ROOT}
     cat > ${WWW_ROOT}/index.html <<- EOM
 <html>
     <head>
