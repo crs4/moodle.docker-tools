f5e11de657badb65561f09e4b75fa84aba0ec195
Marco Enrico Piras <kikkomep@crs4.it>
2016-04-19 17:03:39 +0200

    simplify install script

--- install-modules.sh
+++ install-modules.sh
@@ -1,57 +1,54 @@
 #!/bin/bash
 
+# paths
 CURRENT_DIR=$(pwd)
-export MOODLE_PACKAGES_FOLDER=/opt/moodle-packages
-export MOODLE_WWW=/var/www/html/moodle
-BRANCH="master"
+MOODLE_PACKAGES_FOLDER=/tmp/moodle-packages # temp folder where Moodle packages are downloaded
+export MOODLE_WWW=/Users/kikkomep/Development/Environment/Cytest/Develop/MoodleDocker/www/moodle
 
+# github settings
+GITHUB_USER="kikkomep"
+BRANCH="inst-update"
+USE_SSH=true
+PRESERVE_HISTORY=true
+
+# build github prefix & suffix
+GITHUB_REPOSITORY_SUFFIX=".git"
+GITHUB_REPOSITORY_PREFIX="https://github.com/${GITHUB_USER}/moodle."
+if [[ ${USE_SSH} = true ]]; then
+	GITHUB_REPOSITORY_PREFIX="git@github.com:${GITHUB_USER}/moodle."
+fi
+
+# create the temp folder for downloading packages
 mkdir -p ${MOODLE_PACKAGES_FOLDER}
 
-if [[ ! -d "${MOODLE_WWW}/repository/omero" ]]; then
-	echo "Cloning the 'moodle.omero-repository' module"
-	git clone https://github.com/kikkomep/moodle.omero-repository.git ${MOODLE_WWW}/repository/omero
-	git fetch	
-fi
-echo "Updating the 'moodle.omero-repository' module ..."
-cd ${MOODLE_WWW}/repository/omero
-git checkout ${BRANCH}
-git pull origin ${BRANCH}
+# list of required modules
+MODULES=("omero-repository" "omero-filepicker" "omero-qtypes" "qbank-tag-filter")
 
+# module targets (relative to MOODLE_WWW)
+TARGETS=("repository/omero" "lib/form/omerofilepicker" "question/type" "local/questionbanktagfilter")
 
-if [[ ! -d "${MOODLE_WWW}/question/type/omeromultichoice" ]]; then
-	echo "Cloning the 'moodle.omero-qtypes' module"
-	git clone https://github.com/kikkomep/moodle.omero-qtypes.git ${MOODLE_WWW}/question/type/omeromultichoice
-	git fetch	
-fi
-echo "Updating the 'moodle.omero-qtypes' module..."
-cd ${MOODLE_WWW}/question/type/omeromultichoice
-git checkout ${BRANCH}
-git pull origin ${BRANCH}
-
-
-if [[ ! -d "${MOODLE_WWW}/local/questionbanktagfilter" ]]; then
-	echo "Cloning the 'moodle.qbank-tag-filter.git' module"
-	git clone https://github.com/kikkomep/moodle.qbank-tag-filter.git ${MOODLE_WWW}/local/questionbanktagfilter
-	git fetch	
-fi
-echo "Updating the 'moodle.qbank-tag-filter.git' module ..."
-cd ${MOODLE_WWW}/local/questionbanktagfilter
-git checkout ${BRANCH}
-git pull origin develop
-
-
-if [[ ! -d "${MOODLE_PACKAGES_FOLDER}/filepicker" ]]; then
-	echo "Cloning the 'moodle.qbank-tag-filter.git' module"
-	git clone https://github.com/kikkomep/moodle.omero-filepicker.git ${MOODLE_PACKAGES_FOLDER}/filepicker
-	git fetch	
-	cd ${MOODLE_PACKAGES_FOLDER}/filepicker
+# install or update MODULES
+for i in $(seq 0 1 $((${#MODULES[@]}-1)))
+do
+	MODULE=${MODULES[${i}]}
+	TARGET=${TARGETS[${i}]}
+	GITHUB_REPOSITORY=${GITHUB_REPOSITORY_PREFIX}${MODULE}${GITHUB_REPOSITORY_SUFFIX}
+	MODULE_TEMP_FOLDER=${MOODLE_PACKAGES_FOLDER}/${MODULE}
+	MODULE_TARGET_FOLDER=${MOODLE_WWW}/${TARGET}
+	if [[ ! -d "${MODULE_TEMP_FOLDER}" ]]; then
+		echo "Cloning the 'moodle.${MODULE}' module"
+		git clone ${GITHUB_REPOSITORY} ${MODULE_TEMP_FOLDER}
+		git fetch
+	fi
+	echo "Updating the 'moodle.${MODULE}' module ..."
+	cd ${MODULE_TEMP_FOLDER}
 	git checkout ${BRANCH}
 	git pull origin ${BRANCH}
-	scripts/install	
-else
-	echo "Updating the 'moodle.qbank-tag-filter.git' module..."
-	cd ${MOODLE_PACKAGES_FOLDER}/filepicker
-	git checkout ${BRANCH}
-	git pull origin ${BRANCH}
-	scripts/deploy
-fi
+	rsync -avt ${MODULE_TEMP_FOLDER}/ ${MODULE_TARGET_FOLDER}
+	if [[ ${PRESERVE_HISTORY} = false ]]; then
+		rm -Rf ${MODULE_TARGET_FOLDER}/.git
+	fi
+	if [[ -f ${MODULE_TARGET_FOLDER}/register.sh ]]; then
+		${MODULE_TARGET_FOLDER}/register.sh
+	fi
+done
