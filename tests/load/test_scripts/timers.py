d5b386c2f959c910d616ca81343ff5295a3c0c51
Marco Enrico Piras <kikkomep@crs4.it>
2016-07-15 15:39:26 +0200

    Added method to store timers on a MySQL DB

--- tests/load/test_scripts/timers.py
+++ tests/load/test_scripts/timers.py
@@ -1,9 +1,33 @@
+import os, MySQLdb
+import datetime, time
+from settings import HTTP_CODES, TIMER_TYPE
 
 
-def report_timers(timer_registry, timer_name, response, latency):
-    if timer_registry is not None:
-        if timer_registry:
-            timer_registry[timer_name] = latency
+def append_timer(timer_registry, timer_name, start_time, latency, response):
+    host = os.environ["MYSQL_HOST"]
+    user = os.environ["MYSQL_USER"]
+    passwd = os.environ["MYSQL_PASSWORD"]
+    db = os.environ["LOAD_TEST_DB"]
+    if isinstance(response, int):
+        http_code = response
+    else:
+        http_code = response.status_code if hasattr(response, "status_code") else response.code
+    conn = MySQLdb.connect(host, user, passwd, db)
+    x = conn.cursor()
+    try:
+        timestamp = datetime.datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
+        x.execute("""INSERT INTO result(request,start_time,latency,http_code) VALUES (%s,%s,%s,%s)""",
+                  (timer_name, timestamp, latency, http_code))
+        conn.commit()
+    except MySQLdb.Error, e:
+        try:
+            print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+        except IndexError:
+            print "MySQL Error: %s" % str(e)
+        conn.rollback()
+        return False
+    conn.close()
+    return True
         response_code = response.status_code if hasattr(response, "status_code") else response.code
         if response_code in HTTP_CODES:
             timer_registry["CODE_" + str(response_code)] = 1
