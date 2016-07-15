2958a812fe4a366e1ad024918ab2d95558a89426
Marco Enrico Piras <kikkomep@crs4.it>
2016-07-15 11:46:13 +0200

    Added common settings file

---
+++ tests/load/test_scripts/settings.py
@@ -0,0 +1,9 @@
+import socket
+
+#MOODLE_URL = "http://" + socket.gethostname() + "/moodle"
+MOODLE_URL = "http://mep.crs4.it/moodle"
+
+HTTP_CODES = [200, 300, 301, 302, 303, 304, 400, 401, 403, 404, 405, 406, 408, 500]
+
+#TIMER_TYPE = "analytic"
+TIMER_TYPE = "average"
