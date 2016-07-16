0fea508da3b5cfc8d8358b7e307e1245a7b2aa04
Marco Enrico Piras <kikkomep@crs4.it>
2016-07-16 21:57:27 +0200

    Updated transaction_flow method to the new signature which uses the TimerRegistry object

--- tests/load/test_scripts/login.py
+++ tests/load/test_scripts/login.py
@@ -2,17 +2,19 @@ from common import BaseTransaction
 
 
 class Transaction(BaseTransaction):
-
     def transaction_name(self):
         return "Login"
 
-    def transaction_flow(self):
+    def transaction_flow(self, timer_registry):
         browser = self._make_browser()
-        resp = self._go_to_page(browser, "", timer_name="HomePage", sleep_time=1)
-        resp = self._go_to_page(browser, "login/index.php", timer_name="LoginPage", sleep_time=1)
+        resp = BaseTransaction.go_to_page(timer_registry, browser, "",
+                                          timer_name="HomePage", sleep_time=1)
+        resp = BaseTransaction.go_to_page(timer_registry, browser, "login/index.php",
+                                          timer_name="LoginPage",
+                                          sleep_time=1)
 
         login_form = self._get_form(browser, "login")
-        assert(login_form), "Error: unable to find the LOGIN form"
+        assert (login_form), "Error: unable to find the LOGIN form"
 
         login_form.set_all_readonly(False)
 
