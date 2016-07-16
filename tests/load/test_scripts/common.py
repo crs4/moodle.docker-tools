9a7d2216da790e6768b80152a79ad86c4748973d
Marco Enrico Piras <kikkomep@crs4.it>
2016-07-16 22:29:54 +0200

    Added method to simulate the loading of resources related to a given page

--- tests/load/test_scripts/common.py
+++ tests/load/test_scripts/common.py
@@ -3,6 +3,7 @@ import abc
 import time
 import mechanize
 from os import path
+from bs4 import BeautifulSoup
 from urlparse import urlparse
 from settings import MOODLE_URL
 from timers import TimerRegistry
@@ -50,6 +51,26 @@ class BaseTransaction(object):
     def _build_path(self, relative_url, base_path=MOODLE_URL, params={}):
         p = base_path + "/" + relative_url
         return p
+
+    def load_current_page_resources(self, browser, timer_registry=None):
+        if not browser or not browser.response:
+            return
+        resp = browser.response()
+        if not resp:
+            return
+        data = resp.get_data()
+        if not data:
+            return
+        page = BeautifulSoup(data, "html.parser")
+        for script in page.findAll("script"):
+            if script.get("src"):
+                print "loading %s" % script.get("src")
+                start_time = time.time()
+                browser.open_novisit(script.get("src"))
+                latency = time.time() - start_time
+                if timer_registry:
+                    timer_registry.add_timer("LoadingScript", start_time, latency)
+                    print timer_registry._raw_timers
 
     def run(self):
         print "Custom timer before", self.custom_timers
