da664b8ba45e2b7af25f2230e1a78832e8dd9840
Marco Enrico Piras <kikkomep@crs4.it>
2016-07-16 21:55:25 +0200

    Encapsulated logic to store timers on the TimerRegistry class

--- tests/load/test_scripts/timers.py
+++ tests/load/test_scripts/timers.py
@@ -1,86 +1,45 @@
-import os, MySQLdb
-import datetime, time
-from settings import HTTP_CODES, TIMER_TYPE
+import os
+import MySQLdb
+import socket
+import time
+import datetime
 
 
-def append_timer(timer_registry, timer_name, start_time, latency, response):
-    host = os.environ["MYSQL_HOST"]
-    user = os.environ["MYSQL_USER"]
-    passwd = os.environ["MYSQL_PASSWORD"]
-    db = os.environ["LOAD_TEST_DB"]
-    if isinstance(response, int):
-        http_code = response
-    else:
-        http_code = response.status_code if hasattr(response, "status_code") else response.code
-    conn = MySQLdb.connect(host, user, passwd, db)
-    x = conn.cursor()
-    try:
-        timestamp = datetime.datetime.fromtimestamp(start_time).strftime('%Y-%m-%d %H:%M:%S')
-        x.execute("""INSERT INTO result(request,start_time,latency,http_code) VALUES (%s,%s,%s,%s)""",
-                  (timer_name, timestamp, latency, http_code))
-        conn.commit()
-    except MySQLdb.Error, e:
+class TimerRegistry(object):
+    def __init__(self, transaction):
+        self._transaction = transaction
+        self._raw_timers = []
+        self._transaction_name = str(time.time()) + "@" + socket.gethostname()
+
+    def add_timer(self, timer_name, start_time, latency, response_code=0):
+        self._transaction.custom_timers[timer_name] = latency
+        tc_name = "CODE_" + str(response_code)
+        if not self._transaction.custom_timers.has_key(tc_name):
+            self._transaction.custom_timers[tc_name] = 0
+        self._transaction.custom_timers[tc_name] += 1
+        self._raw_timers.append((timer_name, start_time, latency, response_code))
+
+    def write_timers_to_db(self):
+        host = os.environ["MYSQL_HOST"]
+        user = os.environ["MYSQL_USER"]
+        passwd = os.environ["MYSQL_PASSWORD"]
+        db = os.environ["LOAD_TEST_DB"]
+        conn = MySQLdb.connect(host, user, passwd, db)
+        x = conn.cursor()
         try:
-            print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
-        except IndexError:
-            print "MySQL Error: %s" % str(e)
-        conn.rollback()
-        return False
-    conn.close()
-    return True
-
-
-def report_timers(timer_registry, timer_name, start_time, latency, response=0):
-    print "Print timer before...."
-    print timer_registry
-
-    if TIMER_TYPE == "analytic":
-        report_analytic_timers(timer_registry, timer_name, start_time, latency, response)
-    else:
-        report_average_timers(timer_registry, timer_name, start_time, latency, response)
-    print "Print timer after...."
-    print timer_registry
-
-    append_timer(timer_registry, timer_name, start_time, latency, response)
-
-
-def report_analytic_timers(timer_registry, timer_name, start_time, latency, response = 0):
-    if timer_registry.has_key(timer_name):
-        timer_registry[timer_name] = []
-    timer_registry[timer_name].append(latency)
-    if isinstance(response, int):
-        response_code = response
-    else:
-        response_code = response.status_code if hasattr(response, "status_code") else response.code
-    print response_code
-    if response_code in HTTP_CODES:
-        tc_name = "CODE_" + str(response_code)
-        if not timer_registry.has_key(tc_name):
-            timer_registry[tc_name] = []
-        timer_registry[tc_name].append(1)
-    for code in HTTP_CODES:
-        if code != response_code:
-            tc_name = "CODE_" + str(code)
-            if not timer_registry.has_key(tc_name):
-                timer_registry["CODE_" + str(tc_name)] = []
-            timer_registry["CODE_" + str(tc_name)].append(0)
-
-
-def report_average_timers(timer_registry, timer_name, start_time, latency, response=0):
-    timer_registry[timer_name] = latency
-    if isinstance(response, int):
-        response_code = response
-    else:
-        response_code = response.status_code if hasattr(response, "status_code") else response.code
-    print response_code
-    if response_code in HTTP_CODES:
-        tc_name = "CODE_" + str(response_code)
-        if timer_registry.has_key(tc_name):
-            timer_registry[tc_name] += 1
-        else:
-            timer_registry[tc_name] = 1
-    for code in HTTP_CODES:
-        if code != response_code:
-            tc_name = "CODE_" + str(code)
-            if not timer_registry.has_key(tc_name):
-                timer_registry["CODE_" + str(tc_name)] = 0
+            for timer in self._raw_timers:
+                # timer fields: timer_name, timestamp, latency, http_code
+                timestamp = datetime.datetime.fromtimestamp(timer[1]).isoformat()
+                x.execute(
+                    """INSERT INTO result(transaction,request,start_time,latency,http_code) VALUES (%s,%s,%s,%s,%s)""",
+                    (self._transaction_name, timer[0], timestamp, timer[2], timer[3]))
+            conn.commit()
+        except MySQLdb.Error, e:
+            try:
+                print "MySQL Error [%d]: %s" % (e.args[0], e.args[1])
+            except IndexError:
+                print "MySQL Error: %s" % str(e)
+            conn.rollback()
+            return False
+        conn.close()
+        return True
