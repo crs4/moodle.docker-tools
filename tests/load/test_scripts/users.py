78516de04f3bb18efa4a3316cc2ee63997a77a33
Marco Enrico Piras <kikkomep@crs4.it>
2016-08-23 12:17:56 +0200

    Added utility functions to generate, write and read a list of test users

--- tests/load/test_scripts/users.py
+++ tests/load/test_scripts/users.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python
 import os
 import csv
+import random
+import string
+import socket
 import MySQLdb
 
+# list of user properties
+USER_PROPERTIES = ['username', 'password', 'firstname', 'lastname', 'email', 'cohort1']
 
-def load_from_file(filename="users.csv"):
+
+def load_from_file(filename="users.csv", delimiter=',', skip_header=True):
     result = []
     with open(filename, 'rb') as csvfile:
-        spamreader = csv.reader(csvfile, delimiter="\t")
-        for row in spamreader:
-            result.append(row[0], row[1])
+        reader = csv.DictReader(csvfile, delimiter=delimiter)
+        for user in reader:
+            result.append(user)
     return result
 
 
-def load_form_db(
+def load_from_db(
         host=os.environ["MYSQL_HOST"],
         user=os.environ["MYSQL_USER"],
         passwd=os.environ["MYSQL_PASSWORD"],
@@ -25,3 +31,37 @@ def load_form_db(
     for row in cur.fetchall():
         result.append((row[0], row[1]))
     return result
+
+
+def generate_users(num_of_users=1, user_prefix='testuser', cohort='test_users'):
+    result = []
+    if type(num_of_users) != int:
+        raise ValueError("'num_of_users' must be an integer")
+    hostname = socket.gethostname()
+    for i in range(0, num_of_users):
+        username = ''.join([user_prefix, str(i)])
+        user = {
+            'username': username,
+            'password': generate_password(),
+            'firstname': ''.join(["FN", str(i)]),
+            'lastname': ''.join(["LN", str(i)]),
+            'email': '@'.join([username, hostname]),
+            'cohort1': cohort
+        }
+        result.append(user)
+    return result
+
+
+def write_users(users, filename='users.csv'):
+    with open(filename, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=USER_PROPERTIES)
+        writer.writeheader()
+        for user in users:
+            writer.writerow(user)
+
+
+def generate_password():
+    length = 13
+    chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+    random.seed = (os.urandom(1024))
+    return ''.join(random.choice(chars) for i in range(length))
