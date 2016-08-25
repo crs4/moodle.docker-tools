ed17f9befcaa12520a2cb4bfd3dea717886b3994
Marco Enrico Piras <kikkomep@crs4.it>
2016-08-25 18:18:02 +0200

    Added default filename containing the list of available questions

--- tests/load/test_scripts/settings.py
+++ tests/load/test_scripts/settings.py
@@ -3,6 +3,7 @@ import logging
 
 #MOODLE_URL = "http://" + socket.gethostname() + "/moodle"
 MOODLE_URL = "http://mep.crs4.it/moodle"
+QUESTION_FILENAME = "questions.csv"
 
 HTTP_CODES = [200, 300, 301, 302, 303, 304, 400, 401, 403, 404, 405, 406, 408, 500]
 
