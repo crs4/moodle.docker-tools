488686c1e555f1df86708a48f7b611de495f9c47
Marco Enrico Piras <kikkomep@crs4.it>
2016-09-19 18:36:15 +0200

    Added new script to launch tests

--- tests/run_load_tests.sh
+++ tests/run_load_tests.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 
+# the current path
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# read general configuration
 source ${CURRENT_DIR}/../config.sh
 
-multimech-run load #--results=/tmp/load-test-results
+# configure dataset
+DATASET=$1
+DATASET_FOLDER="${CURRENT_DIR}/datasets/${DATASET}"
+echo "DataSet folder: $DATASET_FOLDER..."
+if [ ! -d ${DATASET_FOLDER} ]; then
+	echo "DataSet ${DATASET} not found: folder ${DATASET_FOLDER} doesn't exist !!!"
+	exit -1
+fi
 
+# remove old links
+rm -r questions.csv users.csv
+# create new links
+ln -s ${DATASET_FOLDER}/questions.csv questions.csv
+ln -s ${DATASET_FOLDER}/users.csv users.csv
+
+shift
+
+# run tests
+locust -f load/locust/FirstTask.py --host=http://omero-test.crs4.it/moodle "$@"
