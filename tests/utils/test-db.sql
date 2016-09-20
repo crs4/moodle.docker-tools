27a2c659cc4c8db8ec3da928c699b714c8fc4aad
Marco Enrico Piras <kikkomep@crs4.it>
2016-09-20 08:47:41 +0200

    Removed old utility scripts

--- tests/utils/test-db.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE result (
-  id          INT(12) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  transaction VARCHAR(100) NOT NULL,
-  request     VARCHAR(100) NOT NULL,
-  start_time  TIMESTAMP(6) NOT NULL,
-  latency     FLOAT        NOT NULL,
-  http_code   INT          NOT NULL
-);
-
