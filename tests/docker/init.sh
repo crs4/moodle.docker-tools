ee4899c1d3838d6ba9c48931b32f4c618afc7221
Marco Enrico Piras <kikkomep@crs4.it>
2016-10-04 18:12:28 +0200

    Added scripts to run load tests within docker containers (crs4/locust image required)

---
+++ tests/docker/init.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# clone the repository
+GIT_TARGET=/home/tester/docker-tools
+git clone https://github.com/kikkomep/moodle.docker-tools.git ${GIT_TARGET}
+cd ${GIT_TARGET}
+git checkout load-tests
+cd tests
+pip install -r requirements.txt
+ln -s /datasets datasets
+
+# script dir
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# the current path
+CURRENT_DIR="${GIT_TARGET}"
+
+# read general configuration
+source ${CURRENT_DIR}/config.sh
+
+# configure dataset
+DATASET=$1
+DATASET_FOLDER="${CURRENT_DIR}/tests/datasets/${DATASET}"
+echo "DataSet folder: $DATASET_FOLDER..."
+if [ ! -d ${DATASET_FOLDER} ]; then
+	echo "DataSet ${DATASET} not found: folder ${DATASET_FOLDER} doesn't exist !!!"
+	exit -1
+fi
+
+# remove old links
+rm -r questions.csv users.csv
+# create new links
+ln -s ${DATASET_FOLDER}/questions.csv questions.csv
+ln -s ${DATASET_FOLDER}/users.csv users.csv
---
+++ tests/docker/start-local.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+IMAGE_NAME="crs4/locust"
+WEB_APP_ADDRESS="http://omero-test.crs4.it/moodle"
+LOCUST_SCRIPT="/home/tester/docker-tools/tests/load/locust_scripts/tests.py"
+INIT_SCRIPT="/scripts/init.sh"
+
+DATASET_FOLDER="$(pwd)/../datasets"
+INIT_SCRIPT_FOLDER="$(pwd)/../docker"
+
+docker run -it \
+    -v ${DATASET_FOLDER}:"/datasets" \
+    -v ${INIT_SCRIPT_FOLDER}:"/scripts" \
+    -p "18086:8086"  \
+    -p "18083:8083"  \
+    -p "18088:8088"  \
+    -p "18089:8089"  \
+    -p "18090:10000" \
+    ${IMAGE_NAME} \
+    start.sh local ${WEB_APP_ADDRESS} ${LOCUST_SCRIPT} ${INIT_SCRIPT}
