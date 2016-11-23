6d49d0265abfda40565aea539039c971901e37f2
Marco Enrico Piras <kikkomep@crs4.it>
2016-11-23 17:05:46 +0100

    Disabled useless settings

--- tests/load/locust_scripts/settings.py
+++ tests/load/locust_scripts/settings.py
@@ -6,7 +6,7 @@ import logging
 
 # MOODLE_URL = "http://" + socket.gethostname() + "/moodle"
 # MOODLE_URL = "http://mep.crs4.it/moodle"
-MOODLE_URL = "http://omero-test.crs4.it/moodle"
+# MOODLE_URL = "http://omero-test.crs4.it/moodle"
 
 # Users
 USERS_FILENAME = 'users.csv'  # default filename containing test users
@@ -19,7 +19,7 @@ QUESTION_FILENAME = "questions.csv"
 HTTP_CODES = [200, 300, 301, 302, 303, 304, 400, 401, 403, 404, 405, 406, 408, 500]
 
 # TIMER_TYPE = "analytic"
-TIMER_TYPE = "average"
+# TIMER_TYPE = "average"
 
 # StatsD server settings
 STATSD_SERVER_ADDRESS = 'localhost'
