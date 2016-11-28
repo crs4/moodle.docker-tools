3921a105936aa5d7bcbc36bdc8a4bf9be5596694
Marco Enrico Piras <kikkomep@crs4.it>
2016-11-28 14:45:05 +0100

    Added new script to launch the locust image

---
+++ images/locust/start.sh
@@ -0,0 +1,153 @@
+#!/usr/bin/env bash
+
+set -e
+
+IMAGE_NAME="crs4/locust"
+WEB_APP_ADDRESS="http://omero-test.crs4.it/moodle"
+LOCUST_SCRIPT="/home/tester/docker-tools/tests/load/locust_scripts/tests.py"
+SETUP_SCRIPT="../../tests/docker/test-setup.sh"
+
+# default TEST
+DATASET="../../tests/datasets/dev"
+
+# default mode
+MODE="-it --rm"
+
+# default volume
+NO_VOLUME="false"
+VOLUME_NAME="locust-influxdb-data"
+
+# parse arguments
+while [ -n "$1" ]; do
+        # Copy so we can modify it (can't modify $1)
+        OPT="$1"
+        # Detect argument termination
+        if [ x"$OPT" = x"--" ]; then
+                shift
+                for OPT ; do
+                        OTHER_OPTS="$OTHER_OPTS \"$OPT\""
+                done
+                break
+        fi
+        # Parse current opt
+        while [ x"$OPT" != x"-" ] ; do
+                case "$OPT" in
+                        # set interactive MODE
+                        -i | --interactive )
+                                MODE="-it --rm"
+                                ;;
+                        # set daemon MODE
+                        -d | --daemon )
+                                MODE="-d"
+                                ;;
+                        # dataset folder
+                        --dataset )
+                                DATASET="$2"
+                                shift
+                                ;;
+                        --dataset=* )
+                                DATASET="${OPT#*=}"
+                                shift
+                                ;;
+                        # set web app
+                        -l=* | --locust-script=* )
+                                LOCUST_SCRIPT="${OPT#*=}"
+                                shift
+                                ;;
+                        -l | --locust-script )
+                                LOCUST_SCRIPT="$2"
+                                shift
+                                ;;
+                        # set web app
+                        -w=* | --web-app=* )
+                                WEB_APP_ADDRESS="${OPT#*=}"
+                                shift
+                                ;;
+                        -w | --web-app )
+                                WEB_APP_ADDRESS="$2"
+                                shift
+                                ;;
+                        # setup script
+                        -s=* | --setup-script=* )
+                                SETUP_SCRIPT="${OPT#*=}"
+                                shift
+                                ;;
+                        -s | --setup-script )
+                                SETUP_SCRIPT="$2"
+                                shift
+                                ;;
+                        # volume name
+                        -v | --volume )
+                                VOLUME_NAME="$2"
+                                shift
+                                ;;
+                        -v=* | --volume=* )
+                                VOLUME_NAME="${OPT#*=}"
+                                shift
+                                ;;
+                        --no-volume )
+                                NO_VOLUME="true"
+                                ;;
+                        # Anything unknown is recorded for later
+                        * )
+                                OTHER_OPTS="$OTHER_OPTS $OPT"
+                                break
+                                ;;
+                esac
+                # Check for multiple short options
+                # NOTICE: be sure to update this pattern to match valid options
+                NEXTOPT="${OPT#-[cfr]}" # try removing single short opt
+                if [ x"$OPT" != x"$NEXTOPT" ] ; then
+                        OPT="-$NEXTOPT"  # multiple short opts, keep going
+                else
+                        break  # long form, exit inner loop
+                fi
+        done
+        # move to the next param
+        shift
+done
+
+
+VOLUME_OPTS=""
+if [[ ${NO_VOLUME} == "false" ]]; then
+    VOLUME_OPTS="-v ${VOLUME_NAME}:/var/lib/influxdb"
+fi
+
+
+#
+if [[ ! -f ${SETUP_SCRIPT} ]]; then
+    echo "Script '${SETUP_SCRIPT}' doesn't exist!"
+    exit -1
+fi
+SETUP_SCRIPT_FOLDER="$( cd "$( dirname "${SETUP_SCRIPT}" )" && pwd )"
+SETUP_SCRIPT=$(basename ${SETUP_SCRIPT})
+
+#
+if [[ ! -d ${DATASET} ]]; then
+    echo "Directory '${DATASET}' doesn't exist!"
+    exit -1
+fi
+DATASET_FOLDER="$( cd ${DATASET} && pwd )"
+DATASET=$(basename ${DATASET})
+
+echo -e "Configuration..."
+echo -e " - Mode: ${MODE}"
+echo -e " - Volume name: ${VOLUME_NAME}"
+echo -e " - Volume disabled: ${NO_VOLUME}"
+echo -e " - WebApp address: ${WEB_APP_ADDRESS}"
+echo -e " - Setup script: ${SETUP_SCRIPT}"
+echo -e " - Setup script path: ${SETUP_SCRIPT_FOLDER}"
+echo -e " - Dataset: ${DATASET}"
+echo -e " - Dataset path: ${DATASET_FOLDER}"
+
+docker run ${MODE} ${VOLUME_OPTS} \
+    -v ${DATASET_FOLDER}:"/datasets" \
+    -v ${SETUP_SCRIPT_FOLDER}:"/scripts" \
+    -p "18086:8086"  \
+    -p "18083:8083"  \
+    -p "18088:8088"  \
+    -p "18089:8089"  \
+    -p "18090:10000" \
+    ${IMAGE_NAME} \
+    -w ${WEB_APP_ADDRESS} -f ${LOCUST_SCRIPT} \
+    -s "/scripts/${SETUP_SCRIPT}" ${DATASET}
