4d516ad579f46b01a7e7644f81abf08a5cfed30c
Marco Enrico Piras <kikkomep@crs4.it>
2016-11-30 21:05:42 +0100

    Parametric functions to collect stats

--- images/locust/stats_utils.sh
+++ images/locust/stats_utils.sh
@@ -2,16 +2,21 @@
 
 # function to collect locust stats
 function collect_locust_stats(){
-    local test_name="${1}"
-    curl -o "${OUTPUT_FOLDER}/${test_name}.csv" "http://localhost:8086/stats/requests/csv"
+    local locust_server_url="${1}"
+    local output_file_prefix="${2}"
+    echo "${locust_server_url}/stats/requests/csv"
+    curl -o "${output_file_prefix}.csv" -G "${locust_server_url}/stats/requests/csv"
 }
 
 # function to collect influxdb stats
 function collect_output(){
-    local test_name="${1}"
-    local info="${2}"
-    influxdb_endpoint="http://localhost:8086/query?pretty=true"
-    curl -o "${OUTPUT_FOLDER}/${test_name}-${info}.json" \
+    local influxdb_server_url="${1}"
+    local output_file_prefix="${2}"
+    local info="${3}"
+    local start_time="${4}"
+    local end_time="${5}"
+    influxdb_endpoint="${influxdb_server_url}/query?pretty=true"
+    curl -o "${output_file_prefix}-${info}.json" \
         -G ${influxdb_endpoint} \
         --data-urlencode "db=telegraf" \
         --data-urlencode "q=SELECT * FROM \"${info}\" WHERE \"time\">'${start_time}' AND \"time\"<'${end_time}'"
@@ -19,13 +24,17 @@ function collect_output(){
 
 # function to collect stats
 function collect_outputs(){
-    local test_name="${1}"
+    local locust_server_url="${1}"
+    local influxdb_server_url="${2}"
+    local output_file_prefix="${3}"
+    local start_time="${4}"
+    local end_time="${5}"
     # collect locust stats
-    collect_locust_stats ${test_name}
+    collect_locust_stats ${locust_server_url} ${output_file_prefix}
     # collect stats from influxdb
     host_info=("cpu" "disk" "diskio" "inode" "io" "mem" "network" "processes")
     for info in "${host_info[@]}"
     do
-        collect_output ${test_name} ${info}
+        collect_output ${influxdb_server_url} ${output_file_prefix} ${info} ${start_time} ${end_time}
     done;
 }
