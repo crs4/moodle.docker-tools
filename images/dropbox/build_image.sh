1a8d5ff6d7fe651a13028e52ad175ab6477119bf
Marco Enrico Piras <kikkomep@crs4.it>
2020-01-07 11:37:01 +0100

    Remove obsolete image

--- images/dropbox/build_image.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-DOCKERHUB_REPOSITORY=${1:-"crs4"}
-DOCKERHUB_IMAGE_NAME=${2:-"moodle-dropbox"}
-
-
-# Build Moodle image
-echo "Building the MoodleDocker image..."
-docker build -t ${DOCKERHUB_REPOSITORY}/${DOCKERHUB_IMAGE_NAME} .
